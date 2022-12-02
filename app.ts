64c53f011eec29a0afdd8c64a4588da12aabb919
yusuf-eren <erenyusuf170@gmail.com>
2022-12-02 21:07:49 +0300

    more readable code

--- app.ts
+++ app.ts
@@ -1,27 +1,40 @@
 import fetch from 'node-fetch';
 import fs from 'fs';
 
-export class DiscordInvite {
-  constructor(private discordToken: string, private channelID: string) {}
+interface InviteLink {
+  path: string;
+  maxUsage: number;
+  numberOfLinks: number;
+  expiresInDays: number;
+}
 
-  async createInviteLink(
-    path: string,
-    maxUses: number,
-    linkCount: number,
-    expiresInDay: number
-  ) {
+interface InviteConstructor {
+  discordToken: string;
+  channelID: string;
+}
+
+export class DiscordInvite {
+  constructor(private AuthOptions: InviteConstructor) {}
+
+  async createInviteLink({
+    numberOfLinks,
+    maxUsage,
+    expiresInDays,
+    path,
+  }: InviteLink) {
+    const { discordToken, channelID } = this.AuthOptions;
     try {
-      const unique = linkCount > 1 ? true : false;
+      const unique = numberOfLinks > 1 ? true : false;
       const requestOptions = {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
-          Authorization: `Bot ${this.discordToken}`,
+          Authorization: `Bot ${discordToken}`,
         },
         body: JSON.stringify({
-          max_uses: maxUses,
+          max_uses: maxUsage,
           unique,
-          max_age: expiresInDay * 86400,
+          max_age: expiresInDays * 86400,
         }),
       };
 
@@ -30,13 +43,13 @@ export class DiscordInvite {
       const fetchDataWithoutBlock = setInterval(async () => {
         checkLimit(x);
         const inviteCode = await fetch(
-          `https://discordapp.com/api/channels/${this.channelID}/invites`,
+          `https://discordapp.com/api/channels/${channelID}/invites`,
           requestOptions
         )
           .then((data: any) => data.json())
           .then((data: any) => data.code);
         fs.appendFile(
-          `${__dirname}/${path}.txt`,
+          `${__dirname}/${path}`,
           `https://discord.gg/${inviteCode}\n`,
           (err: any) => {
             if (err) throw new Error(err);
@@ -47,7 +60,8 @@ export class DiscordInvite {
       }, 5000);
 
       function checkLimit(createdCount: Number) {
-        if (createdCount === linkCount) clearInterval(fetchDataWithoutBlock);
+        if (createdCount === numberOfLinks)
+          clearInterval(fetchDataWithoutBlock);
       }
     } catch (error: any) {
       throw new Error(error);
